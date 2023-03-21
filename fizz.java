18425dfaedbde99d830900a29d5b5a9ed31eb0e8
MineGamer69 <aaryankapoor2003@gmail.com>
2023-03-21 18:55:15 -0400

    implemented fizz buzz

--- fizz.java
+++ fizz.java
@@ -1,5 +1,28 @@
+//import java.util.Scanner;
 public class fizz {
     public static void main(String[] args){
-    }
-    
+        boolean x = true;
+        int p = 0;
+        //Scanner inp = new Scanner(System.in);
+        while (x){
+            if (p < 101){
+                p++;
+                if (p % 3 == 0){
+                    if (p % 5 == 0){
+                        System.out.println("FizzBuzz");
+                    }
+                    System.out.println("Fizz");
+                }
+                if (p % 5 == 0){
+                    System.out.println("Buzz");
+                }
+                
+            }
+            else{
+                x = false;
+
+            }
+        }
+    }  
 }
+
